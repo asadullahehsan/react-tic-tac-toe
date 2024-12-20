f6e39be12d63b27a1566c56ce9aea4fe67ea1ef8
Asadullah Ehsan <asadullahsuri131@gmail.com>
2024-12-20 17:42:45 +0500

    Rewrite Board to use two loops to make the squares instead of hardcoding them.

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -52,21 +52,17 @@ export const Board = ({ xIsNext, squares, onPlay }) => {
     <>
       <div className="status">{status}</div>
 
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {[0, 1, 2].map((rowIndex) => (
+        <div key={rowIndex} className="board-row">
+          {[0, 1, 2].map((columnIndex) => (
+            <Square
+              key={`${rowIndex}-${columnIndex}`}
+              value={squares[rowIndex * 3 + columnIndex]}
+              onSquareClick={() => handleClick(rowIndex * 3 + columnIndex)}
+            />
+          ))}
+        </div>
+      ))}
     </>
   );
 };
